61ed5ba775fde2331cf58b9d921309eb57a2f4b7
Mohit Gera <mohitgera7714@gmail.com>
2024-12-02 21:26:36 +0530

    state management done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{lazy , Suspense, useEffect, useState} from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -6,7 +6,7 @@ import Body from "./components/Body";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Error from "./components/Error";
-import { createBrowserRouter,RouterProvider ,Outlet} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import UserContext from "./utils/UserContext";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
@@ -17,69 +17,70 @@ import Cart from "./components/Cart";
 // import Grocery from "./components/Grocery.js";
 
 // Lazy Loader 
-const Grocery = lazy(()=> import("./components/Grocery"));
-const About = lazy(()=>import("./components/About"));
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
 
 
 const AppLayout = () => {
 
-    const [userName,setUserName] = useState();
-    useEffect(()=>{
-        const data ={
+    const [userName, setUserName] = useState();
+    useEffect(() => {
+        const data = {
             name: "Mohit Gera",
         };
         setUserName(data.name);
-    },[]);
+    }, []);
 
     return (
         <Provider store={appStore}>
-        <UserContext.Provider value={{LoggedInUser:userName,setUserName}}> 
-        <div className="app">
-            <Header />
-            <Outlet />
-        </div>
-        </UserContext.Provider>
+            <UserContext.Provider value={{ LoggedInUser: userName, setUserName }}>
+                <div className="app">
+                    <Header />
+                    <Outlet />
+                </div>
+            </UserContext.Provider>
         </Provider>
     )
 }
 
 const appRoute = createBrowserRouter([
     {
-        path:"/",
-        element:<AppLayout/>,
-        children:[ 
+        path: "/",
+        element: <AppLayout />,
+        children: [
             {
-                path:"/",
-                element:<Body/>
+                path: "/",
+                element: <Body />
             },
             {
-                path:"/About",
-                element:(
-                    <Suspense fallback={<h1>Loading..</h1>}><About/></Suspense>
+                path: "/About",
+                element: (
+                    <Suspense fallback={<h1>Loading..</h1>}><About /></Suspense>
                 )
             },
             {
-                path:"/contact",
-                element:<Contact/>
+                path: "/contact",
+                element: <Contact />
             },
             {
-                path:"/grocery",
-                element:(
-                <Suspense fallback={<h1>Loading..</h1>}><Grocery/> </Suspense>
+                path: "/grocery",
+                element: (
+                    <Suspense fallback={<h1>Loading..</h1>}><Grocery /> </Suspense>
                 )
             },
             {
-                path:"/restaurant/:resId",
-                element:<RestaurantMenu/>
+                path: "/restaurant/:resId",
+                element: <RestaurantMenu />
             },
             {
-                path:"/cart",
-                element:<Cart/>
+                path: "/cart",
+                element: <Cart />
             },
         ],
-        errorElement:<Error/>
+        errorElement: <Error />
     },
 ])
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRoute}/>);
+root.render(<RouterProvider router={appRoute} />);
